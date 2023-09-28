c464ba0ea5df8db44e28d8e069d21d2e906eb830
Matheus <mn909118@gmail.com>
2023-09-28 17:48:33 -0300

    img-v1.4

--- server/excluir.php
+++ server/excluir.php
@@ -12,7 +12,7 @@ $file = $dados['foto'];
 $query = "DELETE FROM usuario WHERE id_usuario=$id";
 $executar = mysqli_query($conexao, $query);
 
-$path = "./img/" . $file;
+$path = "img/" . $file;
 $delete = unlink($path);
 
 if ($delete and $executar) {
