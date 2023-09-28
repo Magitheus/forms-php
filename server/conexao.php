e714dd5bd4b7decae15c93c98ba99465636913dd
Matheus <mn909118@gmail.com>
2023-09-28 17:21:35 -0300

    conexao-v1.5

--- server/conexao.php
+++ server/conexao.php
@@ -5,7 +5,7 @@ $DATABASE = getenv('MYSQLDATABASE') ?? 'revista';
 $PASS = getenv('MYSQLPASSWORD') ?? '1234';
 $PORT = getenv('MYSQLPORT') ?? '3306';
 
-echo ' ' . $HOST .' '.$USER.' '.$PASS .' '. $DATABASE.' '. $PORT';
+echo ' ' . $HOST .' '.$USER.' '.$PASS .' '. $DATABASE.' '. $PORT.'';
 
 $conexao = mysqli_connect($HOST, $USER, $PASS, $DATABASE, $PORT) or die ("Erro ao conectar");
 ?>
