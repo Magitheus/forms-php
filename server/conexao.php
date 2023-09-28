6cdbd7da1502eb27be004ab78b579bd83e2af0de
Matheus <mn909118@gmail.com>
2023-09-28 16:57:58 -0300

    conexao-v1

--- server/conexao.php
+++ server/conexao.php
@@ -1,9 +1,31 @@
-<?php 
-$HOST = getenv('HOST') ?? 'localhost';
-$USER = getenv('USER') ?? 'root';
-$DATABASE = getenv('DATABASE') ?? 'revista';
-$PASS = getenv('PASS') ?? '1234';
-$PORT = getenv('PORT') ?? '3306';
+<?php
+function conectarDB() {
+  $servidor = getenv('DATABASE');
+  $usuario = getenv('HOST');
+  $senha = getenv('PASS');
+  $bd = getenv('DATABASE');
+  $port = getenv('PORT');
 
-$conexao = mysqli_connect($HOST, $USER, $PASS, $DATABASE, $PORT) or die ("Erro ao conectar");
+  if (!$servidor || !$usuario || !$senha || !$bd || !$port) {
+      die("As informações de conexão não foram configuradas corretamente.");
+  }
+
+  $cnx = mysqli_connect("$servidor", "$usuario", "$senha", "$bd", "$port");
+  return $cnx;
+}
+
+$conexao = conectarDB();
+
+if ($conexao) {
+  error_log ("Conectado");
+} else {
+  error_log ("Não conectado");
+}
+// $HOST = getenv('HOST') ?? 'localhost';
+// $USER = getenv('USER') ?? 'root';
+// $DATABASE = getenv('DATABASE') ?? 'revista';
+// $PASS = getenv('PASS') ?? '1234';
+// $PORT = getenv('PORT') ?? 3306;
+
+// $conexao = mysqli_connect($HOST, $USER, $PASS, $DATABASE, $PORT) or die ("Erro ao conectar");
 ?>
