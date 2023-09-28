4f8200bdf2b3da0c1661bb869badefd5be8e5038
Matheus <mn909118@gmail.com>
2023-09-28 16:02:40 -0300

    correcao da conexao

--- server/conexao.php
+++ server/conexao.php
@@ -1,9 +1,9 @@
 <?php 
-$HOST = getenv($HOST) ?? 'localhost';
-$USER = getenv($USER) ?? 'root';
-$DATABASE = getenv($DATABASE) ?? 'revista';
-$PASS = getenv($PASS) ?? '1234';
-$PORT = getenv($PORT) ?? '3306';
+$HOST = getenv(HOST) ?? 'localhost';
+$USER = getenv(USER) ?? 'root';
+$DATABASE = getenv(DATABASE) ?? 'revista';
+$PASS = getenv(PASS) ?? '1234';
+$PORT = getenv(PORT) ?? '3306';
 
 $conexao = mysqli_connect($HOST, $USER, $PASS, $DATABASE, $PORT) or die ("Erro ao conectar");
 ?>
